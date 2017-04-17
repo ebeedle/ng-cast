73daea5ac568975574d273450c2c2d9be831082c
ebeedle <ebeedle@ucla.edu>
2017-04-16 20:28:29 -0700

    new

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,6 +1,16 @@
 angular.module('video-player')
+
+.controller('videoListCtrl', function() {
+	this.videos = window.exampleVideoData
+})
+
 .directive('videoList', function() {
   return {
-    // TODO
+  	scope: {},
+  	restrict: 'E',
+    controller: 'videoListCtrl',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    templateUrl: 'src/templates/videoList.html'
   };
 });
